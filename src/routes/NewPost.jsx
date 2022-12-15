73fbcd2bb7ac755cf99e056dbaca5668ac803821
lauromiottojr <junior.miotto36@gmail.com>
2022-12-14 21:15:48 -0300

    Creating NewPost component

--- src/routes/NewPost.jsx
+++ src/routes/NewPost.jsx
@@ -1,6 +1,21 @@
+import './NewPost.css'
+
 const NewPost = () => {
     return (
-        <div>NewPost</div>
+        <div className="newPost">
+            <h2>Inserir novo post:</h2>
+            <form>
+                <div className="formControl">
+                    <label htmlFor="title">Título:</label>
+                    <input type="text" name="title" id="title" placeholder="Digite o título" />
+                </div>
+                <div className="formControl">
+                    <label htmlFor="body">Conteúdo:</label>
+                    <textarea type="text" name="body" id="body" placeholder="Digite o conteúdo"></textarea>
+                </div>
+                <input type="submit" value="Criar post" className="btn" />
+            </form>
+        </div>
     )
 }
 
